d69d7ca25ebe0af69a1c1dbe1f1af6f8eb626719
iassasin <iassasin@yandex.ru>
2018-01-27 12:49:04 +0300

    add disable autowire feature in container

--- src/SimpleContainer.php
+++ src/SimpleContainer.php
@@ -3,10 +3,12 @@
 namespace Iassasin\Easyroute;
 
 class SimpleContainer implements \Psr\Container\ContainerInterface {
-	private $services;
+	protected $services;
+	protected $autowireEnabled;
 
 	public function __construct(){
 		$this->services = [];
+		$this->autowireEnabled = true;
 	}
 
 	/**
@@ -14,7 +16,15 @@ class SimpleContainer implements \Psr\Container\ContainerInterface {
 	 */
 	public function get($id){
 		if ($this->has($id)){
-			return $this->services[mb_strtolower($id)];
+			return $this->services[$id];
+		}
+
+		if ($this->autowireEnabled && class_exists($id)){
+			// recursion dependency break
+			$this->services[$id] = null;
+			$obj = $this->createInstance($id);
+			$this->services[$id] = $obj;
+			return $obj;
 		}
 
 		throw new ServiceNotFoundException("Service ${id} not found");
@@ -24,10 +34,45 @@ class SimpleContainer implements \Psr\Container\ContainerInterface {
 	 * @inheritdoc
 	 */
 	public function has($id){
-		return array_key_exists(mb_strtolower($id), $this->services);
+		return array_key_exists($id, $this->services);
 	}
 
+	/**
+	 * Does container try to create new service instances by it's name
+	 * @param bool $val
+	 */
+	public function setAutowireEnabled($val){
+		$this->autowireEnabled = $val ? true : false;
+	}
+
+	/**
+	 * Associate service $id with instance $service
+	 * @param string $id
+	 * @param object $service
+	 */
 	public function setService($id, $service){
-		$this->services[mb_strtolower($id)] = $service;
+		$this->services[$id] = $service;
+	}
+
+	protected function createInstance($id){
+		$class = new \ReflectionClass($id);
+		$ctor = $class->getConstructor();
+
+		if ($ctor === null){
+			return new $id();
+		}
+
+		$params = $ctor->getParameters();
+		$args = array_fill(0, count($params), null);
+		foreach ($params as $param){
+			$pclass = $param->getClass();
+			if ($pclass === null){
+				throw new ServiceNotFoundException("Service ${id} requires unknown dependency \$".$param->name);
+			}
+
+			$args[$param->getPosition()] = $this->get($pclass->name);
+		}
+
+		return $ctor->invokeArgs(null, $args);
 	}
 }
