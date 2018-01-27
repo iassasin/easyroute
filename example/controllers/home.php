674003d257a1f935d8cff86cefdf0318510ee8d3
iassasin <iassasin@yandex.ru>
2018-01-27 11:55:20 +0300

    add example for static route

--- example/controllers/home.php
+++ example/controllers/home.php
@@ -5,4 +5,8 @@ class ControllerHome {
 		echo '<html><body>Home page! '.($arg !== null ? 'Argument: '.$arg : 'Argument not set')
 			.'<br><img src="/assets/paper.png"></body></html>';
 	}
+
+	public function staticRoute($arg){
+		echo 'This is static route with arg: '.$arg;
+	}
 }
--- example/routes.php
+++ example/routes.php
@@ -8,6 +8,7 @@ use Iassasin\Easyroute\Route;
 $router = new Router();
 $router->setControllersPath($_SERVER['DOCUMENT_ROOT'].'/controllers/');
 $router->addRoutes([
+	new Route('/static/route', ['controller' => 'home', 'action' => 'staticRoute', 'arg' => 'static arg']),
 	new Route('/{controller}/{action}/{arg}', ['controller' => 'home', 'action' => 'index', 'arg' => null]),
 ]);
-$router->processRoute($_SERVER['REQUEST_URI']);
+$router->processRoute();
